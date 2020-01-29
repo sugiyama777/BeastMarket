702fc9dead96d20c4fbc179258d2154004cbbd18
KazukiSugiyama201910 <sugiyama@freemake.co.jp>
2020-01-29 15:41:05 +0900

    商品一覧画面作成

--- shop/ProductList.php
+++ shop/ProductList.php
@@ -4,3 +4,53 @@ namespace shop;
 class productList {}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>猛獣市場</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+  <!-- ヘッダー -->
+  <div class="header">
+    <div class="header-logo">猛獣市場</div>
+    <div class="header-list">
+      <ul>
+        <li>猛獣市場とは</li>
+        <li>出品商品について</li>
+    </div>
+  </div>
+  <!-- 商品一覧 -->
+  <div class="product-list">
+    <div class="product-item">
+      <img src="./img/skull-937248_1920.jpg">
+      <p class="product-item-name">test</p>
+    </div>
+    <div class="product-item">
+      <img src="./img/skull-1170772_1920.jpg">
+      <p class="product-item-name">test</p>
+    </div>
+    <div class="product-item">
+      <img src="./img/skulls-55485_1920.jpg">
+      <p class="product-item-name">test</p>
+    </div>
+    <div class="product-item">
+      <img src="./img/turtle-281073_1920.jpg">
+      <p class="product-item-name">test</p>
+    </div>
+  </div>
+
+  <!-- フッター -->
+  <div class="footer">
+    <div class="footer-warning">密猟は犯罪です</div>
+    <div class="footer-list">
+      <ul>
+        <li>会社概要</li>
+        <li>採用情報</li>
+        <li>お問い合わせ</li>
+    </div>
+  </div>
+</body>
+</html>
