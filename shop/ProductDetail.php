dfd88a92aa849122007fd50a5235e073c05c8ee7
KazukiSugiyama201910 <sugiyama@freemake.co.jp>
2020-01-29 16:39:50 +0900

    商品詳細画面作成

--- shop/ProductDetail.php
+++ shop/ProductDetail.php
@@ -4,3 +4,48 @@ namespace shop;
 class productDetail {}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>猛獣市場</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+  <!-- ヘッダー -->
+  <div class="header">
+    <div class="header-logo">猛獣市場</div>
+    <div class="header-list">
+      <ul>
+        <li>猛獣市場とは</li>
+        <li>出品商品について</li>
+    </div>
+  </div>
+  <!-- 商品詳細 -->
+  <div class="product-detail">
+    <div class="detail-img">
+      <img src="./img/skull-937248_1920.jpg">
+    </div>
+    <div class="detail-">
+    <div class="detail-name">test</div>
+    <div class="detail-description">
+      testtesttesttesttesttest<br>
+      testtesttesttesttesttest<br>
+      testtesttesttesttesttest<br>
+      testtesttesttesttesttest<br>
+      testtesttesttesttesttest<br>
+    </div>
+  </div>
+  <!-- フッター -->
+  <div class="footer">
+    <div class="footer-warning">密猟は犯罪です</div>
+    <div class="footer-list">
+      <ul>
+        <li>会社概要</li>
+        <li>採用情報</li>
+        <li>お問い合わせ</li>
+    </div>
+  </div>
+</body>
+</html>
